b0beabc01380e48ef9fd35a620048eaa2fd86e30
Mathilde <111729553+mathildeolesen@users.noreply.github.com>
2024-09-19 14:41:30 +0200

    live-5 - frontend oppgave 12 og 13
    * Grid styrer hva som skal skje med dataen som kommer fra AddStudentForm, med funksjonen onAddStudent.
    - Nå så er det å oppdatere den interne listen, students
    - onAddStudent sendes som prop til AddStudentForm, så denne kan trigges derifra
    
    * AddStudentForm får inn data, og "trigger" håndteringen til Grid (onAddStudent)

--- live-5/frontend/src/components/Grid.tsx
+++ live-5/frontend/src/components/Grid.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Student from "./Student"
 import { Student as StudentProps } from "./types";
+import AddStudentForm from "./AddStudentForm";
 
 type GridProps = {
     students: StudentProps[]
@@ -9,7 +10,7 @@ type GridProps = {
 export default function Grid(props: GridProps) {
 
     //const { students } = props;
-    const [students, setStudents] = useState(props.students ?? [])
+    const [students, setStudents] = useState<StudentProps[]>(props.students ?? [])
 
     // React hjelper  å holde kontroll på tilstanden
     // I dette tilfellet ønsker jeg å ha kontroll på studenter
@@ -20,13 +21,25 @@ export default function Grid(props: GridProps) {
     // Denne funksjonen kan du bruke for å oppdatere listen din -> setStudents
     // (setStudents er basert på en intern funksjon i useState)
 
+
+    const onAddStudent = (student: {name: string} ) => {
+        setStudents((prev) => [...prev, {id: crypto.randomUUID(), ...student }]);
+        // (prev) -> staten sånn som den *var*
+        // ...prev -> sprer dem som var der allerede ut
+        // , {id: crypto.randomUUID(), ...student } -> og legger til det nye på slutten
+
+    }
+
     return (
-        <article className="grid">
-            {students.map((student) => (
-                <Student key={student.id} name={student.name} id={student.id}/>
-                // <Student key={student.id} [...student]}/> 
-                // <-- Vet ikke hva vi sender videre, men slipper å forandre navn mange steder
-            ))}
-        </article>
+        <section>
+            <article className="grid">
+                {students.map((student) => (
+                    <Student key={student.id} name={student.name} id={student.id}/>
+                    // <Student key={student.id} [...student]}/> 
+                    // <-- Vet ikke hva vi sender videre, men slipper å forandre navn mange steder
+                ))}
+            </article>
+            <AddStudentForm onAddStudent={onAddStudent}/>
+        </section>
     )
 }
