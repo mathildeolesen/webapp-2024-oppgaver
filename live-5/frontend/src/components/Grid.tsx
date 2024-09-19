3d5e410c13c54f252adf2a6b48abb6ca955242a9
Mathilde <111729553+mathildeolesen@users.noreply.github.com>
2024-09-19 12:57:59 +0200

    live-5 - frontend oppgave 10 og 11
    Bruke State på listen med studenter i Grid.

--- live-5/frontend/src/components/Grid.tsx
+++ live-5/frontend/src/components/Grid.tsx
@@ -1,17 +1,24 @@
+import { useState } from "react";
 import Student from "./Student"
+import { Student as StudentProps } from "./types";
 
-type Student = {
-    name: string
-    id: string
-}
 type GridProps = {
-    students: Student[]
+    students: StudentProps[]
 }
-
 
 export default function Grid(props: GridProps) {
 
-    const { students } = props;
+    //const { students } = props;
+    const [students, setStudents] = useState(props.students ?? [])
+
+    // React hjelper  å holde kontroll på tilstanden
+    // I dette tilfellet ønsker jeg å ha kontroll på studenter
+    // useState krever en initial state -> props.students ?? [] -> props.students ELLER en tom liste
+    // to variabler ut fra useState -> [students, setStudents]
+    // useState returnerer dette --> return [tilstandenDin, funksjonenDuKanBrukeForÅOppdatereTilstanden]
+    // Hva inneholder listen nå -> students
+    // Denne funksjonen kan du bruke for å oppdatere listen din -> setStudents
+    // (setStudents er basert på en intern funksjon i useState)
 
     return (
         <article className="grid">
--- live-5/frontend/src/components/Student.tsx
+++ live-5/frontend/src/components/Student.tsx
@@ -1,9 +1,6 @@
 import Avatar from "./Avatar";
+import { Student as StudentProps } from "./types";
 
-type StudentProps = {
-    id: string
-    name: string
-}
 
 export default function Student(props: StudentProps) {
     const { id, name } = props
